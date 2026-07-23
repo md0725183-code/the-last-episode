15854fcfe8d01128c10c6272e4396814191f5c70
md0725183-code <md0725183@gmail.com>
2026-07-23 16:55:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -248,3 +248,71 @@ Next Scene ➜
 `;
 
 }
+function episode2Scene3(){
+
+document.querySelector(".container").innerHTML=`
+
+<div class="movieScene" style="background:black;min-height:100vh;color:white;text-align:center;padding:20px;">
+
+<img src="photo1.jpg" class="moviePhoto">
+<div style="width:100%;max-width:420px;margin:20px auto 30px auto;">
+
+<div style="
+height:8px;
+background:#333;
+border-radius:20px;
+overflow:hidden;">
+
+<div style="
+width:45%;
+height:100%;
+background:#E50914;">
+</div>
+
+</div>
+
+<p style="
+margin-top:8px;
+color:#aaa;
+font-size:14px;">
+Scene 3 of 7
+</p>
+
+</div>
+
+<h1 style="margin-top:25px;color:#E50914;">
+✨ Your Elegance
+</h1>
+
+<p style="font-size:20px;line-height:1.8;margin-top:20px;">
+It isn't just your beauty...
+<br><br>
+It's the way you carry yourself,
+your grace,
+your kindness,
+and the confidence in every step.
+<br><br>
+You don't just look beautiful...
+you make every moment beautiful.
+❤️
+</p>
+
+<div class="movieControls">
+
+<button onclick="episode2Scene2()"
+style="padding:12px 22px;background:#444;color:white;border:none;border-radius:10px;margin-right:10px;">
+⬅ Previous
+</button>
+
+<button onclick="episode2Scene4()"
+style="padding:12px 22px;background:#E50914;color:white;border:none;border-radius:10px;">
+Next Scene ➜
+</button>
+
+</div>
+
+</div>
+
+`;
+
+}
